11d4cf7faa6c09a61ff0956eff65cd6c9b6f4bcc
Nithin,Ayiluri <nithin.ayiluri@gmail.com>
2022-11-05 17:22:13 -0400

    Adds resistance

--- fitness_machine.py
+++ fitness_machine.py
@@ -188,7 +188,7 @@ class SupportedResistanceLevelRange(Characteristic):
         minvalue = 0.0
         maxvalue = 10.0
         inc = 0.1
-        payload = bytes([0]) + struct.pack('<f', maxvalue) + struct.pack('<f', inc)
+        payload = [struct.pack('<f', minvalue), struct.pack('<f', maxvalue), struct.pack('<f', inc)]
 
         return payload
 
